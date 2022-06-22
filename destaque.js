c43e9d20d5f26456930c896f486267fa3314a1f8
Gustavo <tavim157244@gmail.com>
2022-06-21 21:24:59 -0300

    adicionando arquivos

---
+++ destaque.js
@@ -0,0 +1,35 @@
+const montaFilmes = (data) =>{
+    let dadosHTML = '';
+    let dadosFilmes = JSON.parse(data.target.response)
+    for(let i=0;i<4;i++){
+      var filme = dadosFilmes.results[i];
+      dadosHTML += `<div class="card col-3" id="filmes">
+      <img src="https://image.tmdb.org/t/p/w500${filme.poster_path}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">${filme.title}</h5>
+        <a  class="btn btn-primary botaoDestaque" onclick = detalhe(${filme.id})>Veja mais</a>
+      </div>
+    </div>`
+   
+    }
+  
+  
+    document.getElementById('divFilmes').innerHTML = dadosHTML;
+  }
+  const mostraErro = (data)=>{
+    alert("Erro na requisição");
+  }
+  
+  const init = () =>{
+  
+    let xhr = new XMLHttpRequest();
+    let url = "https://api.themoviedb.org/3/movie/popular?api_key=5dc5fdaea6948e02e3e4deeca0b3aa6b&language=pt-BR"
+    xhr.onload = montaFilmes;
+    xhr.onerror= mostraErro;
+    xhr.open('GET', url, true);
+    xhr.send();
+  
+  
+  }
+  
+  document.onload = init();
---
+++ detalhe.js
@@ -0,0 +1,62 @@
+const filmesDados = {
+
+}
+
+var codFilme = () =>{
+  let input = document.getElementById('pesquisa');
+ 
+const pesquisa = input.value; 
+const listaUser =  localStorage.setItem('bd',pesquisa);
+
+
+window.location.assign("pesquisa.html");
+
+}
+
+const mostraFilmes = (data) =>{
+  let dadosHTML = '';
+  let filmesDados = JSON.parse(data.target.response)
+     let filme = filmesDados;
+     console.log(filme)
+     let dataJson = JSON.stringify(filme.release_date);
+     let datat  = dataJson.replaceAll("-","/");
+     let dataCorreta = JSON.parse(datat)
+    dadosHTML += 
+    `<div id="imagem" style="float:left;">
+    <img src="https://image.tmdb.org/t/p/w500${filme.poster_path}"style="width:90%;" >
+    </div>
+    <div id="descricao" >
+    <h1>${filme.title}</h1>
+    <h2>Sinopse</h2>
+    <p>${filme.overview}</p>
+     <h2>Gênero</h2>
+     <p>${filme.genres[0].name} e ${filme.genres[1].name}</p>
+     <h2>Avaliação</h2>
+     <p>IMDb: ${filme.vote_average}</p>
+      <h2>Data de lançamento</h2>
+      <p>${dataCorreta}</p>
+    
+  </div>`
+
+
+  document.getElementById('detalhe').innerHTML = dadosHTML;
+}
+
+const mostrarErro = (data)=>{
+  alert("Erro na requisição");
+}
+const inic =  () =>{
+  let pesquisa = localStorage.getItem('ids');
+  let xhr = new XMLHttpRequest();
+  let url = `https://api.themoviedb.org/3/movie/${pesquisa}?api_key=5dc5fdaea6948e02e3e4deeca0b3aa6b&language=pt-BR`;
+ 
+  xhr.onload = mostraFilmes;
+  xhr.onerror= mostrarErro;
+  xhr.open('GET', url,true);
+  xhr.send();
+
+}
+
+
+
+document.onload=inic();
